595b05398ce1c7267d9a79e262b590dd54f05b47
Oliver Appel <oliver.appel@topmotive.eu>
2022-05-03 15:49:07 +0200

    Fix Entry class

--- src/indexer.py
+++ src/indexer.py
@@ -7,32 +7,21 @@ HT_DIR = '../html/'
 class Entry:
     def __init__(self, url):
         self.url = url
-        self.title = ''
-        self.date = ''
-
-    def set_title(self, title):
-        self.title = title
-
-    def get_title(self) -> str:
-        return self.title
-
-    def set_date(self, date):
-        self.date = date
+        self.title = None
+        self.date = None
 
 def create_entries(md_dir) -> []:
     """Create list of Entry objects"""
-
     entries = []
-
     for p in Path(md_dir).glob('**/*.md'):
         with p.open() as f:
             lines = f.readlines()
             entry = Entry(p.name)
             for line in lines:
                 if line.startswith('title:'):
-                    entry.set_title(line.split(":"))
+                    entry.title = line.split(":")[1].strip()
                 if line.startswith('date:'):
-                    entry.set_date(line.split(":"))
+                    entry.date = line.split(":")[1].strip()
 
             entries.append(entry)
 
@@ -44,9 +33,6 @@ def main():
     entries = create_entries(MD_DIR)
 
     print(len(entries))
-    
-    for e in entries:
-        print(e.get_title())
 
 
 if __name__ == "__main__":
